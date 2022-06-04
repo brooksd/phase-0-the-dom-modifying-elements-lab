cae9470e7be2e1d97fd9e04b8d579358fd62680b
Donald Brooks Arum <brooksd079@gmail.com>
2022-06-04 19:33:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here 
+const greeting = "hi"
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Donald Brooks is the champion!";
+newHeader.className = "victory";
